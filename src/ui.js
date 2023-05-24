ddd75aa5d58cec1f51077aa9b16446943d597a0a
MJProgramming <joey.meierkord@gmail.com>
2023-05-24 21:25:16 +0200

    Add more styling

--- src/ui.js
+++ src/ui.js
@@ -1,11 +1,5 @@
 import Dates from "./dates";
 
 export default class UI {
-    static constructNav() {}
-    static conctructDateOverview() {
-        const month = Dates.getMonthName();
-        const abreviationMonth = month.substring(0, 2);
-        const year = Dates.getYear();
-        const day = Dates.getDayName();
-    }
+    
 }
