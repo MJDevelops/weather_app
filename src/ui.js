294a3951600d67a3e35d96d76d6077c92e50d4ee
MJProgramming <joey.meierkord@gmail.com>
2023-05-25 23:07:23 +0200

    Add methods for getting search request and displaying data

--- src/ui.js
+++ src/ui.js
@@ -1,5 +1,27 @@
-import Dates from "./dates";
+import WeatherAPI from "./weatherapi";
+import DOMContent from "./domcontent";
 
 export default class UI {
-    
+    static loadUI() {
+        UI.searchBtnEvent();
+    }
+    static searchBtnEvent() {
+        const btn = document.querySelector('.search-btn');
+        btn.addEventListener('click', () => {
+            UI.displayWeather();
+        });
+    }
+    static async displayWeather() {
+        const weatherDisplay = document.querySelector('.weather-card-main');
+        const city = DOMContent.getCitySearch();
+        const condition = await WeatherAPI.getWeatherCondition(city);
+        console.log(condition);
+        weatherDisplay.children[0].textContent = condition.cityName;
+        weatherDisplay.children[1].innerHTML = condition.tempC + '&deg;C';
+        weatherDisplay.children[2].textContent = condition.conditionText;
+    }
+    static constructP(content) {
+        const p = document.createElement('p');
+        p.textContent = content;
+    }
 }
