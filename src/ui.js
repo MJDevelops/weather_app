f8e58b3f5b38d40226503bc3ed48005079d9634b
MJProgramming <joey.meierkord@gmail.com>
2023-05-26 21:11:16 +0200

    Add method for displaying extra weather and displaying error message

--- src/ui.js
+++ src/ui.js
@@ -9,20 +9,55 @@ export default class UI {
         const btn = document.querySelector('.search-btn');
         btn.addEventListener('click', (e) => {
             e.preventDefault();
+            UI.checkForErrorP();
             UI.displayWeather();
+            UI.displayExtraWeatherData();
         });
     }
     static async displayWeather() {
-        const weatherDisplay = document.querySelector('.weather-card-main');
-        const city = DOMContent.getCitySearch();
-        const condition = await WeatherAPI.getWeatherCondition(city);
-        console.log(condition);
-        weatherDisplay.children[0].textContent = condition.cityName;
-        weatherDisplay.children[1].innerHTML = condition.tempC + '&deg;C';
-        weatherDisplay.children[2].textContent = condition.conditionText;
+        try {
+            const weatherDisplay = document.querySelector('.weather-card-main');
+            const city = DOMContent.getCitySearch();
+            const condition = await WeatherAPI.getWeatherCondition(city);
+            weatherDisplay.children[0].textContent = condition.cityName;
+            weatherDisplay.children[1].innerHTML = condition.tempC + '&deg;C';
+            weatherDisplay.children[2].textContent = condition.conditionText;
+            weatherDisplay.classList.remove('hidden');
+        } catch (err) {
+            // mute error
+        }
     }
-    static constructP(content) {
+    static async displayExtraWeatherData() {
+        try {
+            const city = DOMContent.getCitySearch();
+            const data = await WeatherAPI.getExtraWeatherData(city);
+            const weatherDiv = document.querySelector('.extra-weather-info');
+            const windKPH = document.querySelector('.wind-speed');
+            const rainChance = document.querySelector('.rain-chance');
+            const pressure = document.querySelector('.pressure');
+            const uv = document.querySelector('.uv-index');
+
+            windKPH.children[1].textContent = data['windKPH'];
+            rainChance.children[1].textContent = data['chanceRain'];
+            pressure.children[1].textContent = data['pressure'];
+            uv.children[1].textContent = data['uv'];
+            weatherDiv.classList.remove('hidden');
+        } catch (err) {
+            if (!document.querySelector('.error')) {
+                const weatherDiv = document.querySelector('#weather');
+                weatherDiv.appendChild(UI.constructP('There was an error processing your request. Please try again.', 'error'));
+            }
+        }
+    }
+    static constructP(content, className) {
         const p = document.createElement('p');
         p.textContent = content;
+        p.classList.add(className);
+        return p;
+    }
+    static checkForErrorP() {
+        if (document.querySelector('.error')) {
+            document.querySelector('.error').remove();
+        }
     }
 }
